75958c57de3bae07b23e3c4eeb7910b50073e8e4
despoina <121597243+theelusianmysteries@users.noreply.github.com>
2025-01-10 09:28:33 -0600

    Add files via upload
    better one that actually works

--- solve quadriatc.py
+++ solve quadriatc.py
@@ -1,7 +1,35 @@
-import cmath
 ## solve quadratic
-A = int(input("What is your A varible? "))
-B = int(input("What is your B varible? "))
-C = int(input("What is your C varible? "))
-print(-B + cmath.sqrt(B**2 - 4 * A * C) / 2 * A)
-print(-B - cmath.sqrt(B**2 - 4 * A * C) / 2 * A)
+try: ## no letters
+
+    A = float(input("What is your A varible? ")) ## get ^2 value
+    if A == 0: ## check for values that dont work
+        print("cannot put 0 into quadratic")
+    #print(A) testing
+
+    B = float(input("What is your B varible? "))## x value
+    if B == 0: ## check for values that dont work
+        print("cannot put 0 into quadratic")
+    #print(B) testing
+
+    C = float(input("What is your C varible? ")) ## get constant value
+    if C == 0: ## check for values that dont work
+        print("cannot put 0 into quadratic")
+    #print(C) testing
+
+except ValueError:  ## no letters
+    print("Please use real numbers for your varibles. ")
+    exit()
+
+d4AC = float(B ** 2.0 - 4.0 * (A * C)) ## defines discrim
+## i also forgot why theres a d there but its there now
+if d4AC < 0:
+    print("no real solutions")
+else: 
+    sqrt_d4AC = d4AC ** 0.5 ## 1/2 can get weird w order of ops - use 0.5
+
+    dPlus = (-B + sqrt_d4AC) / (2 * A) 
+    dMinus = (-B - sqrt_d4AC) / (2 * A) ## this is only here bc i cant do plus or minus in python
+
+    print(dPlus)
+
+    print(dMinus)
